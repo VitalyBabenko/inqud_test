a2e075389fb4d08ee9985d3de36abd60ea4f4ce2
Vitaly <itassan6@gmail.com>
2023-12-12 18:20:13 +0200

    finished headingSection on post page

--- src/app/insights/[slug]/_components/headingSection/HeadingSection.tsx
+++ src/app/insights/[slug]/_components/headingSection/HeadingSection.tsx
@@ -11,15 +11,17 @@ interface HeadingSection {
 const HeadingSection = ({ title, readingTime, publishDate }: HeadingSection) => {
   return (
     <div className={styles.headingSection}>
+      <Image src="/page-decor-left.svg" width={724} height={360} alt="left-decor" />
+      <Image src="/page-decor-right.svg" width={724} height={360} alt="right-decor" />
       <Link href="/insights">
         <Image src="/arrow.svg" alt="arrow-back" width={16} height={16} />
         Back
       </Link>
       <h1>{title}</h1>
-      <div>
+      <div className={styles.postInfo}>
         <span>{publishDate}</span>
         <Image src="/dot.svg" alt="decor" width={4} height={4} />
-        <span>{readingTime}</span>
+        <span>{readingTime} min read</span>
       </div>
     </div>
   );
