80608ad2520af49276c81609b00165b9218ede12
Vitaly <itassan6@gmail.com>
2023-12-31 14:01:11 +0200

    deploy fix

--- src/app/insights/[slug]/page.tsx
+++ src/app/insights/[slug]/page.tsx
@@ -2,6 +2,8 @@ import HeadingSection from './_components/headingSection';
 import Breadcrumb from '@/_components/breadcrumb';
 import ContentSection from './_components/contentSection';
 
+const URL = process.env.NEXT_PUBLIC_API_URL;
+
 type PostPageProps = {
   params: {
     slug: string;
@@ -9,9 +11,8 @@ type PostPageProps = {
 };
 
 const PostPage = async ({ params: { slug } }: PostPageProps) => {
-  const response = await fetch(`http://localhost:3000/api/posts/${slug}`);
+  const response = await fetch(`${URL}/api/posts/${slug}`);
   const { post } = await response.json();
-  
 
   return (
     <main>
